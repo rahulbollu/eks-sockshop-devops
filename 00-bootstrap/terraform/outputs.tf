6ef7b1fb2b0b0844b4d7f0cad7d23de2f6782e05
rahulbollu <bvrahul3141@gmail.com>
2025-08-29 19:23:28 +0530

    output

--- 00-bootstrap/terraform/outputs.tf
+++ 00-bootstrap/terraform/outputs.tf
@@ -2,10 +2,10 @@ output "cluster_name" {
   value = module.eks.cluster_name
 }
 
-output "argocd_url" {
-  value = "LoadBalancer URL - will be available via: kubectl get svc -n argocd"
+output "argocd_access" {
+  value = "kubectl port-forward svc/argocd-server -n argocd 8080:443"
 }
 
-output "node_group_role" {
-  value = module.eks.eks_managed_node_groups["default"].iam_role_arn
+output "argocd_password_cmd" {
+  value = "kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath=\"{.data.password}\" | base64 -d"
 }
